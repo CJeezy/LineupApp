38b75f50c59e579920830acdd0d15215ae25aad3
CJeezy <cjduma98@gmail.com>
2022-12-09 13:06:10 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [shapes, setShapes] = useState([]);
+
+  const handleCreateSquare = (size) => {
+    setShapes([...shapes, { type: 'square', size, x: 0, y: 0 }]);
+  };
+
+  const handleCreateCircle = (size) => {
+    setShapes([...shapes, { type: 'circle', size, x: 0, y: 0 }]);
+  };
+
+  const handleMoveShape = (index, x, y) => {
+    const newShapes = [...shapes];
+    newShapes[index].x = x;
+    newShapes[index].y = y;
+    setShapes(newShapes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Shapes</h1>
+      <button onClick={() => handleCreateSquare(100)}>Create Square 100px</button>
+      <button onClick={() => handleCreateSquare(50)}>Create Square 50px</button>
+      <button onClick={() => handleCreateCircle(100)}>Create Circle 100px</button>
+      <button onClick={() => handleCreateCircle(50)}>Create Circle 50px</button>
+      {shapes.map((shape, index) => {
+        return shape.type === 'square' ? (
+          <div
+            key={index}
+            style={{
+              width: shape.size,
+              height: shape.size,
+              backgroundColor: 'blue',
+              position: 'absolute',
+              left: shape.x,
+              top: shape.y,
+            }}
+            onMouseMove={(e) => handleMoveShape(index, e.clientX, e.clientY)}
+          />
+        ) : (
+          <div
+            key={index}
+            style={{
+              width: shape.size,
+              height: shape.size,
+              borderRadius: '50%',
+              backgroundColor: 'red',
+              position: 'absolute',
+              left: shape.x,
+              top: shape.y,
+            }}
+            onMouseMove={(e) => handleMoveShape(index, e.clientX, e.clientY)}
+          />
+        );
+      })}
     </div>
   );
 }
